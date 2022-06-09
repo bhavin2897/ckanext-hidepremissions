caa1bd90f9ed98ca9614878a3227ec03f59f60fe
Bhavin Katabathuni <bhavin2897@gmail.com>
2022-06-09 14:10:00 +0200

    Initial commit

---
+++ ckanext/hidepremissions/plugin.py
@@ -0,0 +1,41 @@
+import ckan.plugins as plugins
+import ckan.plugins.toolkit as toolkit
+
+def no_registering(context, data_dict):
+    return {
+        'success': False,
+        'msg': toolkit._(
+            'You cannot register for this site.'
+        )
+    }
+
+class HidepremissionsPlugin(plugins.SingletonPlugin):
+    plugins.implements(plugins.IConfigurer)
+    plugins.implements(plugins.IFacets)
+    plugins.implements(plugins.IAuthFunctions,inherit = True)
+
+    def _facets(self, facets_dict):
+        if 'groups' in facets_dict:
+            del facets_dict['groups']
+        return facets_dict
+
+    def dataset_facets(self, facets_dict, package_type):
+        return self._facets(facets_dict)
+
+    def group_facets(self, facets_dict, group_type, package_type):
+        return self._facets(facets_dict)
+
+    def organization_facets(self, facets_dict, organization_type,
+                            package_type):
+        return self._facets(facets_dict)
+
+    def get_auth_functions(self):
+        return {
+            'user_create': no_registering
+        }
+
+    def update_config(self, config_):
+        toolkit.add_template_directory(config_, 'templates')
+        toolkit.add_public_directory(config_, 'public')
+        toolkit.add_resource('fanstatic',
+            'hidepremissions')
